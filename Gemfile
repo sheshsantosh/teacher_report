ee7ee5a935333dea03ce38d0638f5f09b2862780
Shesh Santosh <shesh.dishari@protonshub.in>
2024-08-16 13:40:42 +0530

    add robocop for indentation

--- Gemfile
+++ Gemfile
@@ -30,7 +30,9 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
-gem 'devise'
+gem "devise"
+gem "rubocop", require: false
+gem "rubocop-rails", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  root to: 'students#index'
+  root to: "students#index"
   resources :students
-  get 'students', to: 'students#index'
+  get "students", to: "students#index"
 
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
